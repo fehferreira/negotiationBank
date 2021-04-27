827c3e119ae27ace4f324c686db9168b8da35fdd
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-26 22:38:23 -0300

    Creating Negociacoes model and implement on index.html

---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,13 @@
+class Negociacoes{
+
+    private _negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao){
+        this._negociacoes.push(negociacao);
+    }
+
+    paraArray(){
+        return this._negociacoes;
+    }
+
+}
