9a2b10bcc1f6c01effae2b248395b8997bfe6c78
Felipe Ferreira <felipe.ferreira1003@gmail.com>
2021-04-27 20:01:50 -0300

    Implementing jQuery  and remove Element

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -1,8 +1,8 @@
 class View {
     constructor(seletor) {
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
     update(model) {
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,12 +1,15 @@
+declare var $: any;
+
 abstract class View<T>{
-    private _elemento: Element;
+
+    private _elemento: any;
 
     constructor(seletor:string){
-        this._elemento = document.querySelector(seletor);
+        this._elemento = $(seletor);
     }
 
     update(model: T){
-        this._elemento.innerHTML = this.template(model);
+        this._elemento.html(this.template(model));
     }
 
     abstract template(model: T): string;
